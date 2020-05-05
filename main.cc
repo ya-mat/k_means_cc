651d147ae4e66f7e66bbeb65fc954effcb153425
fafa <fafa@debian>
2020-05-05 15:10:47 +0900

    ver 1.0

--- main.cc
+++ main.cc
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <Eigen/Dense>
-#include <complex>
- 
+
 int main()
 {
   Eigen::MatrixXd xn;
   Eigen::MatrixXd mu;
   Eigen::VectorXi gk;
+  Eigen::VectorXi numk;
+  Eigen::MatrixXd::Index minIndex;
   int ni;
   int nn;
   int kk;
   int i;
   int j;
   int l;
+  int sum_gk;
+  int bef_sum_gk;
 
-  double tmp;
-  double now;
-  Eigen::VectorXi numk;
-
-  // after :: standard input
+  // after :: from file input
   ni = 10;
   kk = 3;
 
@@ -27,98 +26,46 @@ int main()
 
   xn = Eigen::MatrixXd::Random(2, nn);
   gk = Eigen::VectorXi(nn);
+  numk = Eigen::VectorXi::Zero(kk);
   std::cout << "xn =" << std::endl << xn << std::endl;
 
-  for(j = 1; j < kk; j++){
-    for(i = 0; i < ni; i++){
-      xn(0, j*ni + i) += static_cast<double>(j)*2.0;
-    }
-  }
+  xn.block(0, nn/kk, 1, nn/kk).array() += 2.0;
+  xn.block(0, 2*nn/kk, 1, nn/kk).array() += 4.0;
 
   mu = xn.block(0, 0, 2, kk);
 
   std::cout << "mu =" << std::endl << mu << std::endl;
 
+  bef_sum_gk = 0;
   for(i = 0; i < 100; i++){
     std::cout << "loop =" << i << std::endl;
     for(j = 0; j < nn; j++){
-      now = 0.0;
-      for(l = 0; l < kk; l++){
-	tmp = std::pow((xn(0, j) - mu(0, l)), 2.0)
-	  + std::pow((xn(1, j) - mu(1, l)), 2.0);
-	if(now == 0.0){
-	  now = tmp;
-	  gk(j) = l;
-	} else if(tmp <= now){
-	  now = tmp;
-	  gk(j) = l;
-	}
-      }
+
+// Is which better?
+      (mu.colwise() - xn.col(j)).colwise().squaredNorm().minCoeff(&minIndex);
+//      (xn.col(j).replicate(1, 3) - mu).colwise().squaredNorm().minCoeff(&minIndex);
+
+      gk(j) = minIndex;
     }
-    numk = Eigen::VectorXi::Zero(kk);
+    numk.array() = 0;
     for(l = 0; l < kk; l++){
-      mu(0, l) = 0.0;
-      mu(1, l) = 0.0;
+      mu.col(l).array() = 0.0;
       for(j = 0; j < nn; j++){
 	if(gk(j) == l){
 	  numk(l) += 1;
 	  mu.col(l) += xn.col(j);
 	}
       }
-      mu.col(l) /= numk(l);
+// Is which better?
+      mu.col(l).array() /= static_cast<double> (numk(l));
+//      mu.col(l) /= static_cast<double> (numk(l));
     }
+    sum_gk = gk.sum();
+    if(bef_sum_gk == sum_gk) break;
+    bef_sum_gk = sum_gk;
   }
 
-  std::cout << "xn =" << std::endl << xn << std::endl;
   std::cout << "numk =" << std::endl << numk << std::endl;
   std::cout << "gk =" << std::endl << gk << std::endl;
   std::cout << "mu =" << std::endl << mu << std::endl;
-
-
-//  xn.block(0, nn/kk, 0, 2*nn/kk-1) = xn.block(0, nn/kk, 0, 2*nn/kk-1) + Eigen::MatrixXd::Constant(0, nn/kk, 1.0);
-
-//  std::cout << xn(1, nn-1) << std::endl;
-
-//  xn.block(0, nn/kk, 0, 2*nn/kk-1) += Eigen::MatrixXd::Constant(1, nn/kk, 1.0);
-//  xn.block(0, nn/kk, 0, 2*nn/kk-1) = xn.block(0, nn/kk, 0, 2*nn/kk-1) + Eigen::MatrixXd::Constant(0, nn/kk, 1.0);
-//  xn.array.block(0, 2*nn/kk, 0, 3*nn/kk-1) += 2.0
-
-//  xn.col(0)
-
-//  std::cout << "xn =" << std::endl << xn.row(0) << std::endl;
-//  std::cout << "xn =" << std::endl << xn.row(1) << std::endl;
-
-//  xn.row(0) = xn.row(0) + Eigen::VectorXd()1.0;
-//  xn.row(0) += Eigen::VectorXd::Constant(nn/kk, 1.0);
-//  xn.row(0) += Eigen::MatrixXd::Constant(1, nn/kk, 1.0);
-//  xn.row(1) = xn.row(1) + 2.0;
-
-//  VectorXcd v(3);
-//  v << std::complex<double>(1.0, 2.0), 2, 3;
-//  cout << "v =" << endl << v << endl;
-//  cout << "m * v =" << endl << m * v << endl;
 }
-
-////Boost test
-//#include <vector>
-//#include <iostream>
-//#include <boost/shared_ptr.hpp>
-// 
-//struct Boo{
-//  Boo(){ std::cout<<"Boo "; }
-//  ~Boo(){ std::cout<<"~Boo "; }
-//};
-//typedef boost::shared_ptr<Boo> BooPtr;
-// 
-//int main(){
-//  std::vector<BooPtr> v;
-//  v.push_back(BooPtr(new Boo));
-//  v.push_back(BooPtr(new Boo));
-//}
-
-/*
-int main() {
-  std::cout << "Hello C++ World" << std::endl;
-  return 0;
-}
-*/
